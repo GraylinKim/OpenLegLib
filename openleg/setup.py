cf1063a91a946aa45c01e0e303cfbf0ac41f27b5
Graylin Kim <graylin.kim@gmail.com>
2010-07-12 14:36:50 -0400

    Started a setup.py

---
+++ openleg/setup.py
@@ -0,0 +1,15 @@
+from distutils import setup
+
+"""
+    #TODO: Finish setup.py
+    #TODO: Look into setup.py fields
+    #TODO: Get an email Address
+"""
+setup(name='nyss_openlegislation',
+      version='0.1dev',
+      author='Graylin Kim (shadesofgraylin)',
+      author_email='',
+      license='MIT',
+      description='A python client for the New York State Senate Open Legislation API',
+
+)
